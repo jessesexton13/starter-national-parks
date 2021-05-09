0fa223f8739808b087ae853120fc1b68a51a43ed
Jesse Sexton <jessescse@gmail.com>
2021-05-09 10:05:39 -0400

    updating the dom checkpoint

--- index.js
+++ index.js
@@ -34,3 +34,56 @@ for (let i = 0; i < areas.length; i++) {
   const element = areas[i];
   console.log(element);
 }
+
+const descriptions = document.querySelectorAll(".description-display");
+
+for (let desc of descriptions.values()) {
+  let content = desc.innerText;
+  console.log(content);
+}
+
+for (let desc of descriptions.values()) {
+  let content = desc.innerText;
+
+  if (content.length > 250) {
+    content = content.slice(0, 250);
+    content = content + '<a href="#">...</a>';
+  }
+
+  desc.innerHTML = content;
+}
+
+for (let rating of ratings) {
+  let ratingValue = parseFloat(rating.innerText);
+  if (ratingValue > 4.7) {
+    rating.classList.add("high-rating");
+    rating.classList.remove("value");
+  }
+}
+
+//creating new element with text
+const parks = document.querySelectorAll(".park-display");
+
+const numberParks = parks.length;
+
+const newElement = document.createElement("div");
+
+newElement.innerText = `${numberParks} exciting parks to visit`;
+
+newElement.classList.add("header-statement");
+
+//add element to the page
+const header = document.querySelector("header");
+
+header.appendChild(newElement);
+
+//Removing DOM elements
+
+// Get the parent element of all parks
+// const main = document.querySelector("main");
+
+// Select a single park
+// const park = main.querySelector(".park-display");
+
+// Remove that park
+// main.removeChild(park);
